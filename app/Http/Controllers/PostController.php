6e661609674128af216c0dddd82aebdfbb6792bc
Basmala Ahmed Ali Ibrahim <117600939+basmala249@users.noreply.github.com>
2026-05-12 20:33:19 +0300

    fix authority

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,16 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 class PostController extends Controller
 {
-    
-    
     public function show(Post $post)
-{
-       $post->load('user'); 
-    
-       return view('posts.show', compact('post')); 
-}
-   
+    {
+       
+       if ($post->user_id !== Auth::id()) {
+            abort(403, 'Unauthorized action.');
+            return redirect()->route('posts.index')->with('error', 'You are not authorized to view this post.');
+        }
+
+        $post->load('user');
+        
+        return view('posts.show', compact('post'));
+    }
+
+    // This is where you will add the photo update method later
+    public function update(Request $request, Post $post)
+    {
+        // Logic goes here
+    }
 }
